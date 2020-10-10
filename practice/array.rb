9be7c47c1944cf8a7a9586d48dd1d284226af757
sourabh Rathore <sourabh@quintype.com>
2020-10-10 16:42:29 +0530

    Variable scope understanding

--- practice/array.rb
+++ practice/array.rb
@@ -1,5 +1,9 @@
 BEGIN {
-  
+  'Local variable'=> 'variable can be access Within the function/block and not outside the function, if variable defined in Within function/block',
+  'Instance variable'
+}
+
+END {
 }
 
 class ArrayPractice
@@ -9,6 +13,3 @@ class ArrayPractice
   end
 
 end
-
-END {
-}
