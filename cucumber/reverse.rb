56c659324af020d226b61f0738a6f32be277a9d5
sourabh Rathore <sourabh@quintype.com>
2022-02-28 23:49:57 +0530

    arrayToString and palindrome

--- cucumber/reverse.rb
+++ cucumber/reverse.rb
@@ -1,14 +1,32 @@
 # candidate ID = 91129
 class Interview
+
 	def reverse(arg)
 		rev_str = []
 		str  = arg.split('')
-		j = str.size
-			(0..str.size).each do |i|
+		j = str.size-1
+			(0...str.size).each do |i|
 				rev_str[i] = str[j]
 				j-=1
 			end
-			puts rev_str
+			rev_str
+		end
+
+		def arrayToString(arg)
+			rev = self.reverse(arg)
+			rev_str = ""
+			rev.to_s
+			rev.join("")
+		end
+
+		def palindrome(arg)
+			rev = arrayToString(arg)
+				if arg == rev
+					return true
+				end
 		end
 end
-Interview.new.reverse("sourabh")
+ans = Interview.new
+ans.reverse("nun")
+puts ans.palindrome("nun")
+ans.arrayToString("nun")
